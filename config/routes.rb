7f83de3ee0feea5db88ee3b1af4951626cec7fa6
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-03-10 09:55:06 +0000

    [Change] urlのhomes/aboutをhome/aboutにルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  get 'homes/about' => 'homes#about'
+  get 'home/about' => 'homes#about'
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
